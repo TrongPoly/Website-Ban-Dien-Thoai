b20de40a39f62b286d7e9b85d9ca68ffc78ea16f
TrongPoly <trongnguyen32293@gmail.com>
2023-07-09 14:40:08 +0700

    Giao diện user v1

---
+++ src/main/java/com/fpoly/controller/user/CartController.java
@@ -0,0 +1,13 @@
+package com.fpoly.controller.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cart")
+public class CartController {
+	@RequestMapping("/index")
+	public String index() {
+		return "User/cart";
+	}
+}
